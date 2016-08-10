1e2443a878eee29350927f2a78f8ffcfe5dba447
zhufeng.wang <zhufeng.wang@qunar.com>
2016-08-10 15:57:03 +0800

    修改show warnings命令执行时，在OSC情况下，会出现错误的问题

--- sql/sql_parse.cc
+++ sql/sql_parse.cc
@@ -10629,9 +10629,11 @@ int mysql_execute_statement(
     sprintf(sql_cache_node->execute_time, "%.3f",  
         (double)(start_timer() - timer) / CLOCKS_PER_SEC);
 
-    sql_cache_node->affected_rows = mysql_affected_rows(mysql);
+    //print the warnings only when execute SQL directly
+    if (!sql_cache_node->use_osc)
+        print_warnings(thd, mysql, sql_cache_node);
 
-    print_warnings(thd, mysql, sql_cache_node);
+    sql_cache_node->affected_rows = mysql_affected_rows(mysql);
 
     if (mysql_fetch_thread_id(mysql, &sql_cache_node->thread_id))
         DBUG_RETURN(true);
